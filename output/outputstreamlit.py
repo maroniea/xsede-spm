c331b01dcc8567cfce71538ee9d66b5e1b51389a
Ryan Dwyer <ryanpdwyer@gmail.com>
2021-07-20 14:47:58 -0400

    initial ideas to allow choosing an impedence model in outputstreamlit.py

--- output/outputstreamlit.py
+++ output/outputstreamlit.py
@@ -3,19 +3,48 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 st.title("Connecting impedance model to Capsol Simulations")
+
+# Add nice heading for cantilever
+
 k_cantilever = st.number_input(label="k_cantilever N/m" ,  value=3.5)
 f0 = st.number_input(label= "Resonance Frequency (Fc) Hz" , value= 66500)
+
+# Heading for tip model parameters...
+
 c=st.number_input(label= "C(F)" , value= 4E-14, format="%.3e")
 Czz= st.number_input(label= 'C" (F/m^2)', value= 3.9E-2 , format="%.3e")
 r_alpha= st.number_input(label = "Alpha", value= 0.2 )
-c_sample=st.number_input(label= 'C sample', value=4E-14, format="%.3e")
-r_sample= st.number_input(label= 'R sample', value=1E12, format='%.3e')
+
+# Sample model and parameters...
+
+models = {'RC': dict(Rsample=1E12, Csample=4e-14)}
+
+models = {'RC': dict(parameters= dict(Rsample=1E12, Csample=4e-14),
+                        image=''}
+
+
+selected_model = st.selectbox("Sample Model", list(models.keys()))
+
+for model in models:
+    if selected_model == model:
+        # Show the picture associated with the model
+        model_dict = models[model]
+        for label, defaultValue in model_dict.items():
+            model_dict[label] = st.number_input(label, value=defaultValue, format="%.3e")
+            
+
+
+
+
 Czz_q= (1-r_alpha)*Czz
 dCzz= r_alpha*Czz
+
 Czz_q
 dCzz
+
 curvature_min= f0/(4*k_cantilever)*Czz_q
 curvature_max=f0/(4*k_cantilever)*Czz
+
 curvature_min
 curvature_max
 
@@ -24,6 +53,7 @@ def Hfunc(f, c, c_sample, r_sample):
     z= 1/(1/r_sample + s*c_sample)
     h= (1/(s*c))/(z +(1/(s*c)))
     return h
+
 def hbarfunc(c, c_sample, r_sample, fm, f0):
     hplus= Hfunc(fm+f0,c, c_sample, r_sample)
     hminus=Hfunc(fm-f0, c, c_sample, r_sample)  
