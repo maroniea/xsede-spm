1b634a1dc186b3df5c0f488517a97d1ae257824e
Ryan Dwyer <ryanpdwyer@gmail.com>
2021-07-27 13:55:29 -0400

    Added a rough setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='capsolpy',
+      version='0.1',
+      description='Capacitance solver for an SPM tip',
+      url='https://github.com/maroniea/xsede-spm',
+      author='Emily Maroni and Ryan Dwyer',
+      author_email='maroniea2023@mountunion.edu, dwyerry@mountunion.edu',
+      license='MIT',
+      packages=['capsol'],
+      zip_safe=False)
