71f0bd13bc9b5da68c240879af8a9a31a6e9ab1e
Ryan Dwyer <ryanpdwyer@gmail.com>
2022-03-01 23:29:48 -0500

    Draft of impedance model final output - just need to add LDS, BLDS, experimental outputs

--- output/outputstreamlit.py
+++ output/outputstreamlit.py
@@ -2,12 +2,14 @@
 import streamlit as st
 import output_streamlit_orig
 import impedance
+import impedance_kpfm
 
 
 st.title("Kelvin Probe Force Microscopy Impedance Model")
 
 apps = {"Electrochemical Impedance Fitting": impedance,
         "Original KPFM analysis": output_streamlit_orig, 
+        "KPFM Impedance Model": impedance_kpfm
 }
 
 app = st.selectbox("Choose an application:", list(apps.keys()))
